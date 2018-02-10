42c1505b555562cfd6268ec81579c393039820b1
Palomino <b3mol@hotmail.com>
2018-02-10 02:05:17 +0100

    con links

--- hello.go
+++ hello.go
@@ -2,8 +2,11 @@ package main
 
 import "fmt"
 
+// https://golang.org/doc/install?download=go1.9.4.windows-amd64.msi
+// https://github.com/derekparker/delve/tree/master/Documentation/installation
+// http://mclibre.org/consultar/informatica/lecciones/vsc-git-repositorio.html
 func main() {
-	a := " Arturo is in the town"
+	a := "Arturo is in the town"
 	fmt.Printf("hello, world\n" + a)
 
 }
